23eec8b4af1dae8b07c3481fe586d8b940573d46
Daniela Ciciliano <dann@Danielas-MacBook-Air.local>
2024-04-12 17:20:29 -0600

    Project Created

---
+++ iOSCodeChallenge/iOSCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCodeChallengeApp.swift
+//  iOSCodeChallenge
+//
+//  Created by Daniela Ciciliano on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
